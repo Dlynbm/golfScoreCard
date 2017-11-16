35595590bac76c7e7ad4481429c4ae6852c47df0
iamdlynm <dbum.35@hotmail.com>
2017-11-16 09:06:32 -0700

    first commit

--- weather.js
+++ weather.js
@@ -23,17 +23,20 @@ function set(ht){
         type: "GET",
         url: "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&APPID=9a0bfebddb482e7b0dd96255f3b22f67",
         datatype: "json",
-        success:function(data){
-            var date= new Date(data.dt*1000);
-            $(ht).append("<h5 id='city' class='text'>"+data.name+"</h5>");
-            $(ht).append("<img id='icon' src='Image/"+data.weather[0].icon+".png' width='30' height='30'>");
+        success:function(data) {
+            var date = new Date(data.dt * 1000);
+            $(ht).append("<h5 id='city' class='text'>" + data.name + "</h5>");
+            $(ht).append("<img id='icon' src='Image/" + data.weather[0].icon + ".png' width='30' height='30'>");
             //$(ht).css("background","url(../weather-plugin/_images/"+data.weather[0].icon+".jpg)");
-            var num =data.main.temp-272.15;
+            var num = data.main.temp - 272.15;
             var n = parseInt(num, 10);
-            $(ht).append("<h5 id='temp' class='text'>"+n+"&deg;C</h5>");
-            $(ht).append("<h5 id='desc' class='text'>"+data.weather[0].description+"</h5>");
+            $(ht).append("<h5 id='temp' class='text'>" + n + "&deg;C</h5>");
+            $(ht).append("<h5 id='desc' class='text'>" + data.weather[0].description + "</h5>");
 
-        function tempconvert(valNum) {
-        valNum = parseFloat(valNum);
-        return Math.round(((valNum-273.15)*1.8)+32);
+            function tempconvert(valNum) {
+                valNum = parseFloat(valNum);
+                return Math.round(((valNum - 273.15) * 1.8) + 32);
+            }
+        });
+});
 
