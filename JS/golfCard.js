8fb91bbabd8f19f59e0f92a458854745482b9ca2
iamdlynm <dbum.35@hotmail.com>
2017-12-06 09:30:09 -0700

    Added out/in columns.

--- JS/golfCard.js
+++ JS/golfCard.js
@@ -37,17 +37,28 @@ function buildCard(mytee){
         var holepar = currentCourse.course.holes[c].tee_boxes[mytee].par;
         var gethcp = currentCourse.course.holes[c].tee_boxes[mytee].hcp;
         var getyards = currentCourse.course.holes[c].tee_boxes[mytee].yards;
+
+        if(c == 10) {
+            $(".scorecolumn").append("<div class = 'outtotal column'><div class = 'holeheader'>OUT</div></div>");
+        }
+
         $(".scorecolumn").append("<div id='column" + (Number(c) + 1) + "' class='column'><div class = 'holeheader'>Hole <div>" + (Number(c) + 1) +" </div><div>Par " + holepar +"</div><div class='hcp'>HCP "+ gethcp + "</div> <div class='yards'>Yards "+ getyards + "</div></div>");
 
     }
+
+    $(".scorecolumn").append("<div class = 'intotal column'><div class = 'holeheader'>IN</div></div>");
     $(".scorecolumn").append("<div class = 'totalc column'><div class = 'holeheader'>Total</div></div>");
+
     fillCard();
 }
 
 function fillCard(){
     for(var p = 1; p <= numplayers; p++){
         $(".playercolumn").append("<div id='pl" + p +"'><span class='deletebtn' onclick='deleteplayer("+ p +")'><i class=\"glyphicon glyphicon-remove-circle\" aria-hidden=\"true\"'></i></span><span contenteditable='true' id='pl'>Player</span></span>");
+        $(".outtotal").append("<input type = 'text' class = 'holeinput' id = 'outtotal" + p + "'>");
         $(".totalc").append("<input type = 'text' class = 'holeinput' id = 'totalhole" + p + "'>");
+        $(".intotal").append("<input type = 'text' class = 'holeinput' id = 'intotal" + p + "'>");
+
         for (var h = 1; h <= numholes.length; h++){
             $("#column" + h).append("<input id = 'player" + p +"hole" + h +"' type= 'number' class= 'holeinput' onkeyup = 'updatescore(" + p +")'/>");
         }
@@ -59,6 +70,9 @@ function fillCard(){
 function deleteplayer (playerid) {
     $("#pl" + playerid).remove();
     $("#totalhole"+ playerid).remove();
+    $("#intotal"+ playerid).remove();
+    $("#outtotal"+ playerid).remove();
+
 
     for( var h = 1; h <= numholes.length; h++) {
         $("#player" + playerid + "hole" + h).remove();
@@ -77,15 +91,18 @@ function updatescore(playerid) {
 
 function addplayer(playerid) {
     var numplayers = 1;
-        for(var p = 1; p <= numplayers; p++) {
-            $(".playercolumn").append("<div id='pl" + p + "'><span class='deletebtn' onclick='deleteplayer(" + p + ")'><i class=\"glyphicon glyphicon-remove-circle\" aria-hidden=\"true\"'></i></span><span contenteditable='true' id='pl'>Player</span></span>");
-            $(".totalc").append("<input type = 'text' class = 'holeinput' id = 'totalhole" + p + "'>");
-            for (var h = 1; h <= numholes.length; h++) {
-                $("#column" + h).append("<input id = 'player" + p + "hole" + h + "' type= 'text' class= 'holeinput' onkeyup = 'updatescore(" + p + ")'/>");
-            }
+    for(var p = 1; p <= numplayers; p++) {
+        $(".playercolumn").append("<div id='pl" + p + "'><span class='deletebtn' onclick='deleteplayer(" + p + ")'><i class=\"glyphicon glyphicon-remove-circle\" aria-hidden=\"true\"'></i></span><span contenteditable='true' id='pl'>Player</span></span>");
+        $(".totalc").append("<input type = 'text' class = 'holeinput' id = 'totalhole" + p + "'>");
+        for (var h = 1; h <= numholes.length; h++) {
+            $("#column" + h).append("<input id = 'player" + p + "hole" + h + "' type= 'text' class= 'holeinput' onkeyup = 'updatescore(" + p + ")'/>");
         }
+    }
 
-        }
+}
+
+
+
 
 
 
